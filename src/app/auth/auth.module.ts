da477545df1ea98ffa18f062ed2578a059cc3506
FranklinParker <fparker@deliveryman.com>
2020-06-07 11:33:00 -0400

    save

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SignInComponent, SignUpComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
