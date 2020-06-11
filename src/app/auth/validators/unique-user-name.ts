abbefe635dfadc64ba2129cdce5f8f722735ec6e
FranklinParker <fparker@deliveryman.com>
2020-06-11 11:35:06 -0400

    save

--- src/app/auth/validators/unique-user-name.ts
+++ src/app/auth/validators/unique-user-name.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
-import {HttpClient } from '@angular/common/http';
-import {AbstractControl,
+import {HttpClient} from '@angular/common/http';
+import {
+  AbstractControl,
   AsyncValidator,
   FormControl,
-  ValidationErrors}
+  ValidationErrors
+}
   from '@angular/forms';
 import {Observable} from 'rxjs';
 
@@ -12,10 +14,11 @@ import {Observable} from 'rxjs';
 export class UniqueUserName implements AsyncValidator {
   constructor(private httpClient: HttpClient) {
   }
-  validate = (formControl:FormControl): Observable<ValidationErrors | null> | null =>
-  {
-    const { value } = formControl;
+
+  validate = (formControl: FormControl): Observable<ValidationErrors | null> | null => {
+    const {value} = formControl;
     console.log(this.httpClient);
-    return null;
+    return this.httpClient.post<any>('https://api.angular-email.com/auth/username',
+      {username: value});
   }
 }
