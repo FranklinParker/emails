587b464d1fe7c3bf1772b6cbfdd1790eab05f179
FranklinParker <fparker@deliveryman.com>
2020-06-11 13:55:34 -0400

    add input type

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -9,6 +9,8 @@ import {FormControl} from '@angular/forms';
 export class InputComponent implements OnInit {
   @Input() control:FormControl;
   @Input() label: string;
+  @Input() inputType: string;
+
   constructor() { }
 
   ngOnInit(): void {
