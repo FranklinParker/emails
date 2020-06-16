5d9ab8950ad679b5706b7fe798e1efaca1ccfcaa
FranklinParker <fparker@deliveryman.com>
2020-06-15 21:04:36 -0400

    ermail

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { InboxHomeComponent } from './components/inbox-home/inbox-home.component';
+import { EmailListComponent } from './components/inbox-home/email-list/email-list.component';
+import { EmailShowComponent } from './components/inbox-home/email-show/email-show.component';
+import { EmailCreateComponent } from './components/inbox-home/email-create/email-create.component';
+import { EmailReplyComponent } from './components/inbox-home/email-reply/email-reply.component';
 
 
 @NgModule({
-  declarations: [InboxHomeComponent],
+  declarations: [InboxHomeComponent, EmailListComponent, EmailShowComponent, EmailCreateComponent, EmailReplyComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
