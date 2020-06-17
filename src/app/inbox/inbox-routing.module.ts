5bd99d8fd453cfbd9c32300d70ff564c36b73d61
FranklinParker <fparker@deliveryman.com>
2020-06-17 17:29:01 -0400

    ficx

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {InboxHomeComponent} from './components/inbox-home/inbox-home.component';
 import {PlaceHolderComponent} from './components/inbox-home/place-holder/place-holder.component';
+import {EmailShowComponent} from './components/inbox-home/email-show/email-show.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
       {
         path: '',
         component: PlaceHolderComponent
+      },
+      {
+        path: ':id',
+        component: EmailShowComponent
       }
 
     ]
