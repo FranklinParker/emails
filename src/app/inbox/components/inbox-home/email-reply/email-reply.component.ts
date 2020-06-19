f919f340d64e621db5a4ee8f677af0ac8f25e0af
FranklinParker <fparker@deliveryman.com>
2020-06-18 22:46:50 -0400

    email reply

--- src/app/inbox/components/inbox-home/email-reply/email-reply.component.ts
+++ src/app/inbox/components/inbox-home/email-reply/email-reply.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Email} from '../../../interface/email';
+import {EmailService} from '../../../email.service';
+import {AuthService} from '../../../../auth/auth.service';
 
 @Component({
   selector: 'app-email-reply',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-reply.component.scss']
 })
 export class EmailReplyComponent implements OnInit {
-
-  constructor() { }
+  showModal = false;
+  @Input() email: Email;
+  constructor(private authService: AuthService,
+              private emailService: EmailService) { }
 
   ngOnInit(): void {
+    this.email = {
+      ...this.email,
+      to: this.email.from,
+      from: this.email.to
+    }
   }
 
+  onSendEmail(email: Email) {
+
+  }
 }
